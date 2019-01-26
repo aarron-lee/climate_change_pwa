ca6b8aa8b57645c618a49454a3415b2d76313809
Aarron Lee <aclee91@gmail.com>
2019-01-26 10:33:04 -0500

    package.json configured with start/build commands, basic react dependencies, webpack-config for dev and prod

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const root = document.getElementById('root');
+  if (root) {
+    ReactDOM.render(<div>this is dev!</div>, root);
+  }
+});
---
+++ webpack.common.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'app', 'app.js')
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'assets/[name].[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'app', 'index.template.html'),
+      inject: 'head'
+    })
+  ]
+};
