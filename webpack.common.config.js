8cc19c25cbd4055fcd996758911a09cecd9d8da5
Aarron Lee <aclee91@gmail.com>
2019-01-26 10:40:19 -0500

    add clean-webpack-plugin to remove old assets on build

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// injects scripts into index.html
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,9 @@
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.config.js');
+// cleans out specified directories on build
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(commonConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin(['assets'])]
 });
