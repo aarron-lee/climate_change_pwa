23bd347cb13a21bf160082c15c28643caefd9363
Aarron Lee <aclee91@gmail.com>
2019-01-29 21:35:20 -0500

    basic index and content cards, use isMobile prop for conditional rendering

--- app/Components/Views/App/AppContainer.jsx
+++ app/Components/Views/App/AppContainer.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import Body from 'PresentationalComponents/Body/Body';
 import Card from 'PresentationalComponents/Card/Card';
+import Column from 'PresentationalComponents/Column/Column';
 import Header from 'PresentationalComponents/Header/Header';
-import Main from 'PresentationalComponents/Main/Main';
 import InfoModal from 'ViewComponents/App/InfoModal';
+import IndexItemCard from 'PresentationalComponents/IndexItemCard/IndexItemCard';
+import Main from 'PresentationalComponents/Main/Main';
+import Row from 'PresentationalComponents/Row/Row';
 
 import withIsMobile from 'UtilComponents/withIsMobile';
 
 const AppContainer = ({ isMobile }) => {
+  let indexItemStyle = {};
+  if (isMobile) {
+    indexItemStyle.width = '100%';
+  }
   return (
     <Body>
       <Header>
@@ -16,9 +23,21 @@ const AppContainer = ({ isMobile }) => {
         <InfoModal />
       </Header>
       <Main>
-        <Card style={{ padding: '16px', marginTop: '25px' }}>
-          <img src="/static/food_chart.png" width={450} height={300} />
-        </Card>
+        <Row style={{ justifyContent: 'center', width: '100%' }}>
+          <Column style={isMobile ? { width: '100%' } : {}}>
+            <IndexItemCard style={{ marginTop: '25px', ...indexItemStyle }}>Food</IndexItemCard>
+            <IndexItemCard style={indexItemStyle}>Transport</IndexItemCard>
+            <IndexItemCard style={indexItemStyle}>Energy</IndexItemCard>
+          </Column>
+          {!isMobile && (
+            <Column>
+              <Card style={{ padding: '16px', marginTop: '25px', width: '800px' }}>
+                <h2>Food</h2>
+                <img src="/static/food_chart.png" width={450} height={300} />
+              </Card>
+            </Column>
+          )}
+        </Row>
       </Main>
     </Body>
   );
