276bf2933ffc21afcf473e81e1e2e2753d92faf8
Aarron Lee <aclee91@gmail.com>
2019-01-29 23:53:41 -0500

    crop food chart, update food page

--- app/Components/Views/App/CategoryContent/Food.jsx
+++ app/Components/Views/App/CategoryContent/Food.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const Food = () => (
   <div>
     <p>Food page</p>
-    <img src="/static/food_chart.png" width="100%" height="auto" style={{ maxWidth: '600px' }} />
+    <figure>
+      <img src="/static/food_chart.png" width="100%" height="auto" style={{ maxWidth: '600px' }} />
+      <figcaption>
+        From BBC{' '}
+        <a
+          href="https://www.bbc.com/news/science-environment-46459714"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          source
+        </a>
+      </figcaption>
+    </figure>
   </div>
 );
 
